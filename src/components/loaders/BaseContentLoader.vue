c5ee86048d98a09a7e0f2edb0395c7c5d81af95a
Moses kamau <moses.kamau@churpy.co>
2022-03-25 04:30:17 +0000

    BaseContentLoader.vue edited online with Bitbucket

--- src/components/loaders/BaseContentLoader.vue
+++ src/components/loaders/BaseContentLoader.vue
@@ -7,12 +7,16 @@
 </script>
 
 <template>
+<div class="flex flex-col hover:cursor-progress w-screen h-screen m-auto">
+
   <content-loader
     viewBox="0 0 450 350"
     :speed="2"
     primaryColor="#ebebeb"
     secondaryColor="#ffffff"
+   
   >
     <slot></slot>
   </content-loader>
+</div>
 </template>
