c5d5db6410cb1443671b9c78b12af88de6db352d
moses kamau <moseskamau338@gmail.com>
2022-04-05 13:20:16 +0300

    Basic table design done

--- src/components/parts/navList.vue
+++ src/components/parts/navList.vue
@@ -14,7 +14,7 @@
             
         </div>
             
-        <router-link v-else :to="{name:item.route}" :class="[$route.name === item.route ? 'bg-gradient-to-br from-churpy-green via-green-600 to-emerald-600 text-white shadow-sm mb-4' : 'text-indigo-100 hover:bg-green-400/30 mb-2', 'group flex items-center px-2 py-1 text-xs rounded-md transition-all hover:translate-x-1']">
+        <router-link v-if="!item.header && !item.disabled" :to="{name:item.route}" :class="[$route.name === item.route ? 'bg-gradient-to-br from-churpy-green via-green-600 to-emerald-600 text-white shadow-sm mb-4' : 'text-indigo-100 hover:bg-green-400/30 mb-2', 'group flex items-center px-2 py-1 text-xs rounded-md transition-all hover:translate-x-1']">
             <i :class="item.icon" class="mr-3 flex-shrink-0"></i>
             {{ item.title }}
         </router-link>
--- src/navigation/recon/recon.js
+++ src/navigation/recon/recon.js
@@ -10,7 +10,6 @@ export default [
   {
     title: 'Audit',
     route: 'audit',
-    disabled:true,
     icon: 'fa-solid fa-ballot-check',
   },
    {
