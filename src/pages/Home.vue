5bb4f2b53242875845b775857de0986c406c95e9
moses kamau <moseskamau338@gmail.com>
2022-06-21 11:24:39 +0300

    dashboard done, services halfway

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,178 @@
 <template>
- <div class="bg-white dark:bg-churpy-night-box md:mx-5 mb-14 min-h-[500px] rounded-md shadow-md pl-3 pt-4 relative">
-   <p class="text-lg mt-2">We're setting up the Developer Portal.</p>
+  <header class="flex justify-between">
+    <Combobox v-model="selected">
+      <div class="relative mt-1">
+        <label class="my-3">Select project</label>
+        <div
+          class="relative md:w-fit cursor-default overflow-hidden rounded-md bg-white dark:bg-churpy-night-box dark:text-gray-500 text-left shadow focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm"
+        >
+          <ComboboxInput
+            class="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 dark:bg-churpy-night-box dark:text-gray-300 focus:ring-0"
+            :displayValue="(person) => person.name"
+            @change="query = $event.target.value"
+          />
+          <ComboboxButton
+            class="absolute inset-y-0 right-0 flex items-center pr-2"
+          >
+           <i class="fa-solid fa-sort text-gray-400 dark:text-gray-600"></i>
+          </ComboboxButton>
+        </div>
+        <TransitionRoot
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          @after-leave="query = ''"
+        >
+          <ComboboxOptions
+            class="absolute mt-1 max-h-60 w-56 overflow-auto rounded-md bg-white dark:bg-churpy-night-box dark:text-gray-500 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+          >
+            <div
+              v-if="filteredPeople.length === 0 && query !== ''"
+              class="relative cursor-default select-none py-2 px-4 text-gray-700 dark:text-gray-500"
+            >
+              Nothing found.
+            </div>
 
- </div>
+            <ComboboxOption
+              v-for="person in filteredPeople"
+              as="template"
+              :key="person.id"
+              :value="person"
+              v-slot="{ selected, active }"
+            >
+              <li
+                class="relative cursor-default select-none py-2 pl-10 pr-4"
+                :class="{
+                  'bg-churpy-green text-white': active,
+                  'text-gray-900 dark:text-gray-300': !active,
+                }"
+              >
+                <span
+                  class="block truncate"
+                  :class="{ 'font-medium': selected, 'font-normal': !selected }"
+                >
+                  {{ person.name }}
+                </span>
+                <span
+                  v-if="selected"
+                  class="absolute inset-y-0 left-0 flex items-center pl-3"
+                  :class="{ 'text-white': active, 'text-teal-600': !active }"
+                >
+                  <i class="fa-solid fa-check"></i>
+                </span>
+              </li>
+            </ComboboxOption>
+          </ComboboxOptions>
+        </TransitionRoot>
+      </div>
+    </Combobox>
+
+    <span class="w-fit">
+      <label>Date range</label>
+       <litepie-datepicker />
+    </span>
+  </header>
+
+  <div class="bg-white dark:bg-churpy-night-box py-3 px-4 mt-5 rounded shadow">
+    <header class="flex justify-between">
+      <h2 class="font-bold text-lg">Statistics</h2>
+      <div class="flex space-x-2">
+        <i class="fa-solid text-sm text-churpy-green fa-check-circle"></i>
+        <span class="text-sm">Project live on Sanbox</span>
+      </div>
+    </header>
+
+    <div class="py-5 mt-4 flex justify-between items-baseline">
+      <div class="flex items-center space-x-12">
+        <!--item 1-->
+        <div class="flex items-center">
+          <span class="bg-green-500/20 text-churpy-green h-12 w-12 flex justify-center items-center rounded-full">
+            <i class="fa-solid fa-wave-pulse"></i>
+          </span>
+          <div class="flex flex-col ml-4">
+            <span class="font-bold text-lg">0</span>
+            <small>Successful API calls</small>
+          </div>
+        </div>
+        <!--item 2-->
+        <div class="flex items-center">
+          <span class="bg-red-500/20 text-red-500 h-12 w-12 flex justify-center items-center rounded-full">
+            <i class="fa-solid fa-arrow-trend-down"></i>
+          </span>
+          <div class="flex flex-col ml-4">
+            <span class="font-bold text-lg">0</span>
+            <small>Failed API calls</small>
+          </div>
+        </div>
+      </div>
+
+      <c-button variant="success">View Project &rightarrow;</c-button>
+    </div>
+
+  </div>
+
 
 
 </template>
 
 <script>
-import {inject} from "vue";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxButton,
+  ComboboxOptions,
+  ComboboxOption,
+  TransitionRoot,
+} from '@headlessui/vue'
+import LitepieDatepicker from 'litepie-datepicker-tw3';
+import {inject, ref, computed} from "vue";
+import CButton from "@/components/parts/CButton.vue";
 
 export default {
   name:'Dashboard',
+  components:{
+    CButton,
+    Combobox,
+    ComboboxInput,
+    ComboboxButton,
+    ComboboxOptions,
+    ComboboxOption,
+    TransitionRoot,
+    LitepieDatepicker,
+  },
   setup(){
-    const axios = inject('axiosInstance')
+    //const axios = inject('axiosInstance')
+    const people = [
+      { id: 1, name: 'Wade Cooper' },
+      { id: 2, name: 'Arlene Mccoy shjdvfsjhdgv sdhgvsdjsdvs dfjsgdvsj' },
+      { id: 3, name: 'Devon Webb' },
+      { id: 4, name: 'Tom Cook' },
+      { id: 5, name: 'Tanya Fox' },
+      { id: 6, name: 'Hellen Schmidt' },
+    ]
 
-    axios('/developer')
-    .then(({data}) => {
-      console.log('Axios response: ', data)
-    })
+    let selected = ref(people[0])
+    let query = ref('')
+
+    let filteredPeople = computed(() =>
+      query.value === ''
+        ? people
+        : people.filter((person) =>
+            person.name
+              .toLowerCase()
+              .replace(/\s+/g, '')
+              .includes(query.value.toLowerCase().replace(/\s+/g, ''))
+          )
+    )
+
+    //axios('/developer')
+    //.then(({data}) => {
+    //  console.log('Axios response: ', data)
+    //})
+
+    return {
+      selected, filteredPeople, query, people
+    }
 
   }
 }
--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -1,11 +1,110 @@
 <template>
-<h1>Services</h1>
+<div class="">
+  <div class="sm:flex sm:items-center">
+    <div class="sm:flex-auto">
+      <h1 class="text-md font-semibold">Integrations Overview</h1>
+      <p class="mt-2 text-sm">This list shows all the <b>banks</b> integrated in <b>Churpy</b> and will be updated from time to time.</p>
+    </div>
+  </div>
+  <div class="-mx-4 mt-5 ring-1 ring-gray-300 dark:ring-gray-600 sm:-mx-6 md:mx-0 md:rounded-md">
+    <table class="min-w-full divide-y divide-gray-300 dark:divide-gray-600">
+      <thead>
+        <tr>
+          <th colspan="1" class="border-r dark:border-r-gray-600"></th>
+          <th colspan="4" class="hidden px-3 py-3.5 text-left text-sm font-semibold lg:table-cell border-b dark:border-b-gray-600">Services</th>
+        </tr>
+        <tr>
+          <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold sm:pl-6 border-r dark:border-r-gray-600">Bank</th>
+          <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold lg:table-cell">Notifications</th>
+          <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold lg:table-cell">Full Statement</th>
+          <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold lg:table-cell">Mini Statement</th>
+          <th scope="col" class="hidden px-3 py-3.5 text-left text-sm font-semibold lg:table-cell">Balance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="index+service.bank" v-for="(service, index) in services">
+          <td class="relative py-4 pl-4 sm:pl-6 pr-3 text-sm border-r dark:border-r-gray-600">
+            <div class="font-medium">{{ service.bank }}</div>
+            <div class="mt-1 flex flex-row flex-wrap w-64 md:w-full text-gray-500 sm:block lg:hidden">
+              <Badge :key="api.key" v-for="api in [
+                  {key:'notifications', label: 'Notifications'},
+                  {key:'full_statements', label: 'Full Statements'},
+                  {key:'mini_statements', label: 'Mini Statements'},
+                  {key:'balance', label: 'Balance'},
+              ]" :status="service[api.key] ? 'success' : 'danger'" class="w-fit mx-2 my-1">
+                {{api.label}}
+              </Badge>
+            </div>
+          </td>
+          <td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell">
+            <span
+                :class="[
+                    service.notifications ? 'bg-green-500/20 text-churpy-green' : 'bg-red-500/20 text-red-500'
+                ]"
+                class="h-8 w-8 flex justify-center items-center rounded-full">
+              <i :class="[service.notifications ? 'fa-check' : 'fa-close']" class="fa-solid"></i>
+            </span>
+          </td>
+          <td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell">
+            <span
+                :class="[
+                    service.full_statements ? 'bg-green-500/20 text-churpy-green' : 'bg-red-500/20 text-red-500'
+                ]"
+                class="h-8 w-8 flex justify-center items-center rounded-full">
+              <i :class="[service.full_statements ? 'fa-check' : 'fa-close']" class="fa-solid"></i>
+            </span>
+          </td>
+          <td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell">
+            <span
+                :class="[
+                    service.mini_statements ? 'bg-green-500/20 text-churpy-green' : 'bg-red-500/20 text-red-500'
+                ]"
+                class="h-8 w-8 flex justify-center items-center rounded-full">
+              <i :class="[service.mini_statements ? 'fa-check' : 'fa-close']" class="fa-solid"></i>
+            </span>
+          </td>
+          <td class="hidden px-3 py-3.5 text-sm text-gray-500 lg:table-cell">
+            <span
+                :class="[
+                    service.balance ? 'bg-green-500/20 text-churpy-green' : 'bg-red-500/20 text-red-500'
+                ]"
+                class="h-8 w-8 flex justify-center items-center rounded-full">
+              <i :class="[service.balance ? 'fa-check' : 'fa-close']" class="fa-solid"></i>
+            </span>
+          </td>
+        </tr>
+
+      </tbody>
+    </table>
+  </div>
+</div>
 
 </template>
 
 <script>
+import Badge from "@/components/parts/Badge.vue";
 export default {
-  name: "Services"
+  name: "Services",
+  components: {Badge},
+  setup(){
+    let services = [
+      {bank:"CITI Bank",
+        //services
+        notifications: true, full_statements: true, mini_statements: false, balance: true,},
+      {bank:"Co-operative Bank",
+        //services
+        notifications: true, full_statements: true, mini_statements: true, balance: true,},
+
+      {bank:"NCBA Bank",
+        //services
+        notifications: false, full_statements: false, mini_statements: false, balance: true,},
+      {bank:"Equity Bank",
+        //services
+        notifications: true, full_statements: true, mini_statements: true, balance: true,}
+
+    ]
+    return {services}
+  }
 }
 </script>
 
